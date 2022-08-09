0627c1abfb95ae4f81ef94d7ffe54d0df230cfcf
Daniel Rodriguez <dirs21@gmail.com>
2022-08-09 19:42:07 -0400

    First commit

---
+++ src/components/OrderItem.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import '@styles/OrderItem.scss';
+import icon_close from '@icons/icon_close.png';
+import AppContext from '../context/AppContext';
+
+const OrderItem = ({ product }) => {
+
+	const { removeFromCart } = useContext(AppContext);
+
+	const handleRemove = itemProduct => {
+		removeFromCart(itemProduct);
+	}
+
+	return (
+		<div className="OrderItem">
+			<figure>
+				<img src={ product.images[0] } alt={ product.title } />
+			</figure>
+			<p>{ product.title }</p>
+			<p>${ product.price }</p>
+			<img src={icon_close} alt="close" onClick={() => handleRemove(product)} />
+		</div>
+	);
+}
+
+export default OrderItem;
